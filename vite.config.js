481c281c58db7bb6f32bcad4ab9419b48c7b38ee
akaiwang523 <akaiwang523@gmail.com>
2026-01-11 22:37:45 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // GitHub Pages 部署時，請將 'equipment-booking-system' 改成你的 repo 名稱
-  base: '/equipment-booking-system/',
+  // ▼▼▼ 修改這一行，前後都要有斜線 ▼▼▼
+  base: '/machine/',
 })
